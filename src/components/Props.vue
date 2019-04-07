17d0f6821e13ae405236647c7ac8749c3d18ca53
zhangtj <1771988873@qq.com>
2019-04-07 15:23:29 +0800

    demo add eg

---
+++ src/components/Props.vue
@@ -0,0 +1,52 @@
+<template>
+		<div>
+				name: {{name}},
+				<br>
+				type: {{type}},
+				<br>
+				list: {{list}}
+				<br>
+				isVisible: {{isVisible}}
+				<br>
+				<button @click="handleClick">change type</button>
+		</div>
+</template>
+
+<script>
+    export default {
+        name: "props",
+        // inheritAttrs: false,
+        props: {
+            name: String,
+            type: {
+                validator: function (value) {
+                    return ["success","warning","danger"].includes(value)
+                }
+            },
+            list: {
+                type: Array,
+                default: () => []
+            },
+            isVisible: {
+                type: Boolean,
+                default: false
+            },
+            onChange: {
+                type: Function,
+                default: () => {}
+            }
+        },
+        methods: {
+            handleClick() {
+                // this.type = "warning"
+                this.onChange(this.type === 'success'?'warning':'success')
+            }
+        }
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+		<!--<li>{{msg}} {{todo.id}}</li>-->
+		<li class="item">
+				<!--{{list}}-->
+				<input type="checkbox" v-model="isCheck">
+				<slot name="item" :isCheck="isCheck" v-bind="{aaa}"></slot>
+		</li>
+</template>
+
+<script>
+    export default {
+        name: "todoItem123",
+        props: ['list'],
+        data() {
+            return{
+                isCheck: false,
+                aaa: 'haha'
+            }
+        }
+        // props: {
+        //     msg: String,
+        // 		todo: Object
+        // }
+    }
+</script>
+
+<style scoped>
+		.item{
+				color: orange;
+		}
+</style>
